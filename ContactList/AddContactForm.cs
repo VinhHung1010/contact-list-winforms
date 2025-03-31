970417a6efe879f11e373cfebd257d65289a9566
Phan Hoang Gia Hung <thoihung15@gmail.com>
2025-03-31 19:57:33 +0700

    Sửa đổi AddContactForm: Thêm Label 'Thông tin bổ sung'

---
+++ ContactList/AddContactForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactList
+{
+    public partial class AddContactForm : Form
+    {
+        public AddContactForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string hoTen = txtHoTen.Text;
+            string soDienThoai = txtSoDienThoai.Text;
+
+            if (string.IsNullOrWhiteSpace(hoTen) || string.IsNullOrWhiteSpace(soDienThoai))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            MessageBox.Show($"Đã lưu liên hệ: {hoTen} - {soDienThoai}", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            this.Close(); // Đóng Form sau khi lưu
+        
+    }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
